295c933b4c2d34730417d2ad41c8bbbb815eea65
Jeron Aldaron Lau <jeron.lau@plopgrizzly.tech>
2017-04-28 22:08:11 -0500

    Updated documentation style.

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,10 @@
  * Copyright 2017 (c) Jeron Lau - Licensed under the MIT LICENSE
 **/
 
+pub const VERSION : &'static str = "adi_clock 0.1.2";
+
 mod clock;
 mod timer;
-
-pub const VERSION : &'static str = "adi_clock 0.1.0";
 
 pub use clock::Clock;
 pub use clock::Pulse;
